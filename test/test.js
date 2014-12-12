7f42425d7edce03b2f84aa45e9f600d46ffe9ad5
jonschlinkert <dev@sellside.com>
2014-12-12 13:00:56 -0500

    adds tests for verb, assemble

--- test/test.js
+++ test/test.js
@@ -8,31 +8,62 @@
 'use strict';
 
 var should = require('should');
+var assemble = require('assemble');
+var verb = require('verb');
 var engine = require('..');
 
-describe('.render()', function() {
-  it('should process a LESS string.', function(done) {
-    var str = '@red: #900;\n.foo {color: @red;}';
-    engine.render(str, function (err, css) {
-      css.should.equal('.foo {\n  color: #990000;\n}\n');
-      done();
-    });
-  });
+// describe('.render()', function() {
+//   it('should process a LESS string.', function(done) {
+//     var str = '@red: #900;\n.foo {color: @red;}';
+//     engine.render(str, function (err, css) {
+//       css.should.equal('.foo {\n  color: #990000;\n}\n');
+//       done();
+//     });
+//   });
 
-  it('should return an object if `lessRenderMode` is `object`', function(done) {
-    var str = '@red: #900;\n.foo {color: @red;}';
-    engine.render(str, {lessRenderMode: 'object'}, function (err, css) {
-      css.should.have.property('css', '.foo {\n  color: #990000;\n}\n');
-      done();
-    });
-  });
-});
+//   it('should return an object if `lessRenderMode` is `object`', function(done) {
+//     var str = '@red: #900;\n.foo {color: @red;}';
+//     engine.render(str, {lessRenderMode: 'object'}, function (err, css) {
+//       css.should.have.property('css', '.foo {\n  color: #990000;\n}\n');
+//       done();
+//     });
+//   });
+// });
 
-describe('.renderFile()', function() {
-  it('should process a LESS file.', function(done) {
-    engine.renderFile('test/fixtures/styles.less', function (err, css) {
+// describe('.renderFile()', function() {
+//   it('should process a LESS file.', function(done) {
+//     engine.renderFile('test/fixtures/styles.less', function (err, css) {
+//       css.should.equal('.aaa {\n  color: blue;\n}\n');
+//       done();
+//     });
+//   });
+// });
+
+describe('assemble usage', function() {
+  it('should work with assemble', function(done) {
+    assemble.engine('less', require('..'));
+    // this isn't necessary, but it's more semantic than using `assemble.page()`
+    assemble.create('style', {isRenderable: true});
+    assemble.styles('test/fixtures/styles.less');
+
+    assemble.render('styles.less', function (err, css) {
       css.should.equal('.aaa {\n  color: blue;\n}\n');
       done();
     });
   });
 });
+
+
+// describe('verb usage', function() {
+//   it('should work with verb', function(done) {
+//     verb.engine('less', require('..'));
+//     // this isn't necessary, but it's more semantic than using `verb.page()`
+//     verb.create('style', {isRenderable: true});
+//     verb.styles('test/fixtures/styles.less');
+
+//     verb.render('styles.less', function (err, css) {
+//       css.should.equal('.aaa {\n  color: blue;\n}\n');
+//       done();
+//     });
+//   });
+// });
