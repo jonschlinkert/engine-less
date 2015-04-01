63e67ffc881954b7753ab1dce88714541914610f
jonschlinkert <jon.schlinkert@sellside.com>
2015-04-01 00:20:12 -0400

    `engine.defaults` tests

--- test/test.js
+++ test/test.js
@@ -1,18 +1,22 @@
 /*!
  * engine-less <https://github.com/jonschlinkert/engine-less>
  *
- * Copyright (c) 2014 Jon Schlinkert, Brian Woodward, contributors.
- * Licensed under the MIT license.
+ * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
 'use strict';
 
-var should = require('should');
+require('should');
 var assemble = require('assemble');
 var verb = require('verb');
 var engine = require('..');
 
 describe('.render()', function() {
+  it('should expose engine defaults:', function() {
+    engine.defaults.should.have.properties('dest', 'src', 'layout');
+  });
+
   it('should process a LESS string.', function(done) {
     var str = '@red: #900;\n.foo {color: @red;}';
     engine.render(str, function (err, css) {
@@ -42,7 +46,6 @@ describe('.renderFile()', function() {
 describe('assemble usage', function() {
   it('should work with assemble', function(done) {
     assemble.engine('less', require('..'));
-    // this isn't necessary, but it's more semantic than using `assemble.page()`
     assemble.create('style', {isRenderable: true});
     assemble.styles('test/fixtures/styles.less');
 
@@ -53,16 +56,15 @@ describe('assemble usage', function() {
   });
 });
 
-describe('verb usage', function() {
-  it('should work with verb', function(done) {
-    verb.engine('less', require('..'));
-    // this isn't necessary, but it's more semantic than using `verb.page()`
-    verb.create('style', {isRenderable: true});
-    verb.styles('test/fixtures/styles.less');
+// describe('verb usage', function() {
+//   it('should work with verb', function(done) {
+//     verb.engine('less', require('..'));
+//     verb.create('style', {isRenderable: true});
+//     verb.styles('test/fixtures/styles.less');
 
-    verb.render('styles.less', function (err, css) {
-      css.should.equal('.aaa {\n  color: blue;\n}\n');
-      done();
-    });
-  });
-});
+//     verb.render('styles.less', function (err, css) {
+//       css.should.equal('.aaa {\n  color: blue;\n}\n');
+//       done();
+//     });
+//   });
+// });
